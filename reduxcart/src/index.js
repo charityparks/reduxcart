b48645b1763996a830e5e40f13e4f8b761d24d8d
charityparks <tuppercharity@gmail.com>
2021-10-07 16:15:55 -0400

    created index js and imports

---
+++ reduxcart/src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import cartReducer from "./reducer";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
