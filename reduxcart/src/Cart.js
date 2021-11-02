2e5c87b8f0947d590495d749194d622e3d61f106
charityparks <tuppercharity@gmail.com>
2021-11-02 15:11:17 -0400

    added Table to cart.js

--- reduxcart/src/Cart.js
+++ reduxcart/src/Cart.js
@@ -6,7 +6,16 @@ class Cart extends Component {
     render() {
         <div className="container">
             <AddProduct addProduct={this.props.onAddProduct}/>
-            
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Product Price</th>
+                        <th>#</th>
+                    </tr>
+                </thead>
+            </Table>
+        
         </div>
     }
 }
