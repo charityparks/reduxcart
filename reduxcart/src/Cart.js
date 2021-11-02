1ede4f34e13ce6b7370339e6189451819f1f2e82
charityparks <tuppercharity@gmail.com>
2021-11-02 15:25:01 -0400

    finished Cart.js

--- reduxcart/src/Cart.js
+++ reduxcart/src/Cart.js
@@ -4,6 +4,7 @@ import { Table } from 'reactstrap';
 
 class Cart extends Component {
     render() {
+        return (
         <div className="container">
             <AddProduct addProduct={this.props.onAddProduct}/>
             <Table>
@@ -20,12 +21,14 @@ class Cart extends Component {
                             <td>{productData.productName}</td>
                             <td>{productData.productPrice}</td>
                             <td> onClick={ () =>
-                        this.props.onDeleteProduct(productData)}>Remove</td>
+                        this.props.onDeleteProduct(productData)}Remove</td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
-        
+           <span>Total Amount: {this.props.totalCost}</span>
         </div>
-    }
-}
+        );
+                            }
+                        };
+export default Cart;
