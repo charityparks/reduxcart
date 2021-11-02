b382de6cff171c408af101be48e882c1c5a717d2
charityparks <tuppercharity@gmail.com>
2021-11-02 15:09:17 -0400

    started container div in cart.js

--- reduxcart/src/Cart.js
+++ reduxcart/src/Cart.js
@@ -2,4 +2,11 @@ import React, { Component} from "react";
 import AddProduct from './AddProduct';
 import { Table } from 'reactstrap';
 
-class Cart extends Component
+class Cart extends Component {
+    render() {
+        <div className="container">
+            <AddProduct addProduct={this.props.onAddProduct}/>
+            
+        </div>
+    }
+}
